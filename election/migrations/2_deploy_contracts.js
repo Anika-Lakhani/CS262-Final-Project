3bc026ad26c842732089ecd3c74182fa14b20786
Anika Lakhani <anikalakhani@college.harvard.edu>
2023-05-02 21:55:17 -0400

    new version please work

--- election/migrations/2_deploy_contracts.js
+++
@@ -1,7 +0,0 @@
-// We reference the smart contract Election.sol as Election
-var Election = artifacts.require("./Election.sol");
-
-// Adds Election to the manifest of deployed contracts to ensure deployment when we run migrations
-module.exports = function(deployer) {
-  deployer.deploy(Election);
-};
